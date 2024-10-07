61756f1eab1f090d0cdf56cce9e570a12dfff423
Ryan Pool <rlpool06@gmail.com>
2024-10-07 02:41:11 -0500

    create staging, change background blue

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Home() {
   const data = await getTriviaData();
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-slate-100">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-200">
       <CardContainer data={data} />
     </main>
   );
